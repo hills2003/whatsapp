833ce993c45a17d484943a7475805f998bbfb441
hillary <75283158+hills2003@users.noreply.github.com>
2021-04-29 16:45:18 -1200

    new ones

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Contain from "./whatsapp/contain"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Contain />
   </React.StrictMode>,
   document.getElementById('root')
 );
